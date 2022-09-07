a456c0cc43724ba9770d7e7890b44ccfc8399930
Mattias Mucherie <mattias.mattias.m@gmail.com>
2022-09-07 15:35:14 +0200

    Add validation and coorps on games

--- pages/api/create-user.ts
+++ pages/api/create-user.ts
@@ -1,12 +1,30 @@
 import type { NextApiRequest, NextApiResponse } from "next"
 import prisma from "../../lib/prisma"
+import { string, ValidationError } from "yup"
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const user = await prisma.user.create({
-    data: { name: req.body.name },
-  })
-  res.status(200).json({ name: user.name })
+  if (req.method === "POST") {
+    try {
+      const nameSchema = string().required()
+      const name = await nameSchema.validate(req.body.name, { strict: true })
+      const user = await prisma.user.create({
+        data: { name },
+      })
+      return res.status(200).json({ name: user.name })
+    } catch (e) {
+      if (e instanceof ValidationError) {
+        return res.status(400).json({ message: e.message })
+      }
+      console.error(e)
+      return res.status(500).json({ message: "Something went wrong" })
+    }
+  } else {
+    res.setHeader("Allow", "POST")
+    return res
+      .status(405)
+      .json({ message: `HTTP method ${req.method} is not supported.` })
+  }
 }

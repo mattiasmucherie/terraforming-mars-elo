f981254c44523b179862a0e0ce40166e3bd76f56
Cornelius Ruckman <cr930326@gmail.com>
2022-09-13 14:53:02 +0200

    Added heading prop to Layout

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,15 +1,24 @@
 import React, { FC, ReactNode } from "react"
 import Header from "./Header"
-import { Box, Flex } from "@chakra-ui/react"
+import { Box, Flex, Heading } from "@chakra-ui/react"
 import Footer from "./Footer"
 
-const Layout: FC<{ children: ReactNode; fullWidth?: boolean }> = ({
-  children,
-  fullWidth,
-}) => {
+type LayoutProps = {
+  children: ReactNode
+  fullWidth?: boolean
+  heading?: string
+}
+
+const Layout: FC<LayoutProps> = ({ children, fullWidth, heading }) => {
   return (
     <Flex flexDirection="column">
       <Header />
+
+      {heading && (
+        <Heading fontSize="18px" p={4} pb={0}>
+          {heading}
+        </Heading>
+      )}
 
       <Box flexGrow={1} pt={4} pb={6} px={fullWidth ? 0 : 4}>
         {children}

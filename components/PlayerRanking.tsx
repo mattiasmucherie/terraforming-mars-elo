cfb3e6e9a95406ec82f21fde4c19bbcf708cfefc
Mattias Mucherie <mattias.mattias.m@gmail.com>
2022-09-14 17:44:50 +0200

    Change rank on home page

--- components/PlayerRanking.tsx
+++ components/PlayerRanking.tsx
@@ -15,7 +15,14 @@ import Link from "next/link"
 import { FC } from "react"
 import { FullWidthContainer } from "./Layout"
 
-const PlayerRanking: FC<{ users: User[] }> = ({ users }) => {
+interface PlayerRankingProps {
+  users: User[]
+  isWithoutLeader?: boolean
+}
+const PlayerRanking: FC<PlayerRankingProps> = ({
+  users,
+  isWithoutLeader = false,
+}) => {
   if (!users || users.length === 0) {
     return <div>Looks like there are no users here...</div>
   }
@@ -33,28 +40,31 @@ const PlayerRanking: FC<{ users: User[] }> = ({ users }) => {
           </Thead>
 
           <Tbody>
-            {users.map((u, i) => (
-              <Tr key={u.name}>
-                <Td>
-                  <Link href={`/user/${u.id}`}>
-                    <Flex
-                      gap={2}
-                      alignItems="center"
-                      sx={{ cursor: "pointer" }}
-                    >
-                      <Avatar
-                        size="xs"
-                        name={u.name}
-                        src={u.image || undefined}
-                      />
-                      <Text>{u.name}</Text>
-                    </Flex>
-                  </Link>
-                </Td>
-                <Td isNumeric>{Math.round(u.rank)}</Td>
-                <Td isNumeric>{i + 1}</Td>
-              </Tr>
-            ))}
+            {users.map((u, i) => {
+              const standingIndex = isWithoutLeader ? 2 : 1
+              return (
+                <Tr key={u.name}>
+                  <Td>
+                    <Link href={`/user/${u.id}`}>
+                      <Flex
+                        gap={2}
+                        alignItems="center"
+                        sx={{ cursor: "pointer" }}
+                      >
+                        <Avatar
+                          size="xs"
+                          name={u.name}
+                          src={u.image || undefined}
+                        />
+                        <Text>{u.name}</Text>
+                      </Flex>
+                    </Link>
+                  </Td>
+                  <Td isNumeric>{Math.round(u.rank)}</Td>
+                  <Td isNumeric>{i + standingIndex}</Td>
+                </Tr>
+              )
+            })}
           </Tbody>
         </Table>
       </TableContainer>

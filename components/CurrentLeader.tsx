9c5c4cf9d71af7b8b0fa8fe4c9c3ffaa8ed30862
Cornelius Ruckman <cr930326@gmail.com>
2022-09-15 17:26:25 +0200

    Adjusted sizes on CurrentLeader

--- components/CurrentLeader.tsx
+++ components/CurrentLeader.tsx
@@ -21,7 +21,7 @@ const CurrentLeader: FC<{ user: User }> = ({ user }) => {
   const theme = useTheme()
 
   return (
-    <Box p="3" pb="2" borderWidth="1px" borderRadius="lg">
+    <Box p="3" pb="3" borderWidth="1px" borderRadius="lg">
       <Box display="flex" alignItems="flex-start">
         <Flex flexGrow="1">
           <Box flexGrow={1}>
@@ -29,26 +29,26 @@ const CurrentLeader: FC<{ user: User }> = ({ user }) => {
               <Badge fontSize="9px" mb="2" mt="1">
                 Current leader
               </Badge>
-              <Heading fontSize="26px">{user.name}</Heading>
+              <Heading fontSize="24px">{user.name}</Heading>
             </Box>
 
             <Divider mt="3" mb="3" />
 
             <StatGroup>
-              <Stat>
+              <Stat size="sm">
                 <StatLabel>Score</StatLabel>
                 <StatNumber>{Math.round(user.rank)}</StatNumber>
               </Stat>
 
-              <Stat>
+              <Stat size="sm">
                 <StatLabel>Rank</StatLabel>
                 <StatNumber>#1</StatNumber>
               </Stat>
             </StatGroup>
           </Box>
 
-          <Avatar ml="5" mr="1" size="lg" src={user.image || undefined}>
-            <AvatarBadge boxSize="1.25em" bg={theme.colors.supernovaOrange}>
+          <Avatar ml="5" mr="1" mt="2" size="md" src={user.image || undefined}>
+            <AvatarBadge boxSize="1.20em" bg={theme.colors.supernovaOrange}>
               <FontAwesomeIcon icon={faStar} color="white" fontSize="0.45em" />
             </AvatarBadge>
           </Avatar>

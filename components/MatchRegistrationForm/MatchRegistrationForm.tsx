05b1df3558c70153d13a3d3fe17b03b61e7e68fc
Cornelius Ruckman <cr930326@gmail.com>
2022-09-30 14:48:10 +0200

    Added tabs

--- components/MatchRegistrationForm/MatchRegistrationForm.tsx
+++ components/MatchRegistrationForm/MatchRegistrationForm.tsx
@@ -1,201 +1,71 @@
-import { DeleteIcon } from "@chakra-ui/icons"
+import { ArrowForwardIcon } from "@chakra-ui/icons"
 import {
-  Box,
   Button,
-  Center,
-  Divider,
-  IconButton,
-  InputGroup,
-  InputLeftAddon,
-  List,
-  ListItem,
-  NumberDecrementStepper,
-  NumberIncrementStepper,
-  NumberInput,
-  NumberInputField,
-  NumberInputStepper,
-  OrderedList,
-  Select,
-  Spinner,
-  Stat,
-  Text,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
 } from "@chakra-ui/react"
-import { StatArrow } from "@chakra-ui/stat"
 import { Corporation, User } from "@prisma/client"
-import axios from "axios"
-import { useRouter } from "next/router"
-import { ChangeEvent, FC, useEffect, useMemo, useState } from "react"
+import { FC, useState } from "react"
 import styled from "styled-components"
 
-import { elo } from "../../utils/elo"
+import { FullWidthContainer } from "../Layout"
 import PlayerSelector from "./PlayerSelector"
 
 const Container = styled.div`
   display: flex;
   flex-flow: column;
-  gap: 2rem;
 `
 
 interface MatchFormProps {
   users: User[]
   corporations: Corporation[]
 }
-type RankedPlayer = User & { corporationId?: string; victoryPoints?: number }
 
-const MatchRegistrationForm: FC<MatchFormProps> = ({ users, corporations }) => {
+const MatchRegistrationForm: FC<MatchFormProps> = ({ users }) => {
   const [selectedPlayers, setSelectedPlayers] = useState<User[]>([])
-  const [rankingOfPlayers, setRankingOfPlayers] = useState<RankedPlayer[]>([])
-  const [loading, setLoading] = useState(false)
-  const router = useRouter()
-  const newElo = useMemo(
-    () => elo(rankingOfPlayers.map((p) => p.rank)),
-    [rankingOfPlayers]
-  )
+  const [tabIndex, setTabIndex] = useState<number>(0)
 
-  useEffect(() => {
-    console.log("selectedPlayers", selectedPlayers)
-  }, [selectedPlayers])
+  const handleTabsChange = (index: number) => setTabIndex(index)
 
-  // Not very good UX but let's rebuild this whole component soon...
-  useEffect(() => {
-    setRankingOfPlayers([])
-  }, [users])
-
-  const handleCorpChange = (
-    e: ChangeEvent<HTMLSelectElement>,
-    p: RankedPlayer
-  ) => {
-    const corp = corporations.find((c) => c.name === e.target.value)
-    if (corp) {
-      setRankingOfPlayers((prevState) => {
-        const index = prevState.findIndex((ps) => ps.id === p.id)
-        return prevState.map((uc, i) => {
-          if (i === index) {
-            return { ...uc, corporationId: corp.id }
-          }
-          return uc
-        })
-      })
-    }
-  }
-
-  const handleOnClickRemove = (u: User) => {
-    setRankingOfPlayers((prevState) => prevState.filter((p) => p.id !== u.id))
-  }
-
-  const handleSubmit = async () => {
-    setLoading(true)
-    try {
-      const listOfNames = rankingOfPlayers.map((p) => ({
-        name: p.name,
-        corporationId: p.corporationId,
-        victoryPoints: p.victoryPoints,
-      }))
-      await axios.post("/api/match/new", listOfNames)
-      router.push("/")
-    } catch (e) {
-      console.error(e)
-    } finally {
-      setLoading(false)
-    }
-  }
-
-  const handleNumberInputChange = async (e: string, p: RankedPlayer) => {
-    const vPoints = Number(e)
-    if (vPoints) {
-      setRankingOfPlayers((prevState) => {
-        const index = prevState.findIndex((ps) => ps.id === p.id)
-        return prevState.map((uc, i) => {
-          if (i === index) {
-            return { ...uc, victoryPoints: vPoints }
-          }
-          return uc
-        })
-      })
-    }
-  }
   return (
     <Container>
-      <PlayerSelector
-        players={users}
-        onSelectedPlayersChanged={setSelectedPlayers}
-      />
+      <FullWidthContainer>
+        <Tabs
+          isFitted
+          colorScheme="supernovaOrange"
+          index={tabIndex}
+          onChange={handleTabsChange}
+        >
+          <TabPanels>
+            <TabPanel>
+              <PlayerSelector
+                players={users}
+                onSelectedPlayersChanged={setSelectedPlayers}
+              />
 
-      {/* <OrderedList>
-          {rankingOfPlayers.map((p, i) => (
-            <ListItem key={p.id} pb="3">
-              <Box
-                display="flex"
-                alignItems="center"
-                justifyContent="space-between"
-                pr="0"
-                pb="1"
-              >
-                <Box display="flex" gap={1}>
-                  <Text>{p.name}</Text>
-                  {newElo.length > 1 && (
-                    <Stat>
-                      <StatArrow
-                        type={
-                          Math.round(newElo[i] - p.rank) > 0
-                            ? "increase"
-                            : "decrease"
-                        }
-                      />
-                      {Math.round(Math.round(newElo[i] - p.rank))}
-                    </Stat>
-                  )}
-                </Box>
-                <IconButton
-                  aria-label={`Remove ${p.name} from list`}
-                  size="sm"
-                  colorScheme="red"
-                  icon={<DeleteIcon />}
-                  onClick={() => handleOnClickRemove(p)}
-                />
-              </Box>
-              <Select
-                placeholder="Select corp"
-                size="sm"
-                onChange={(e) => handleCorpChange(e, p)}
-              >
-                {corporations.map((c) => (
-                  <option key={c.id} value={c.name}>
-                    {c.name}
-                  </option>
-                ))}
-              </Select>
-              <InputGroup my={1}>
-                <InputLeftAddon>VP</InputLeftAddon>
-                <NumberInput
-                  defaultValue={0}
-                  min={0}
-                  max={200}
-                  width="100px"
-                  allowMouseWheel
-                  onChange={(e) => handleNumberInputChange(e, p)}
-                  placeholder="Victory points"
-                >
-                  <NumberInputField
-                    borderTopLeftRadius={0}
-                    borderBottomLeftRadius={0}
+              <Button
+                w="100%"
+                mt={4}
+                rightIcon={
+                  <ArrowForwardIcon
+                    style={{ position: "relative", top: 0.5 }}
                   />
-                  <NumberInputStepper>
-                    <NumberIncrementStepper />
-                    <NumberDecrementStepper />
-                  </NumberInputStepper>
-                </NumberInput>
-              </InputGroup>
-            </ListItem>
-          ))}
-        </OrderedList> */}
+                }
+                onClick={() => handleTabsChange(1)}
+              >
+                Next
+              </Button>
+            </TabPanel>
 
-      {/* <Button
-        onClick={handleSubmit}
-        disabled={loading || rankingOfPlayers.length < 3}
-      >
-        {loading ? <Spinner /> : "Submit New Match"}
-      </Button> */}
+            <TabPanel>
+              <p>{selectedPlayers.map((p) => p.name)}</p>
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </FullWidthContainer>
     </Container>
   )
 }

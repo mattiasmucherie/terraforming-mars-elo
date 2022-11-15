1f253617e3bbbe352682d34ae82914ff5801a506
Mattias Mucherie <mattias.mattias.m@gmail.com>
2022-11-15 14:06:42 +0100

    Remove any from layout stuff

--- components/with-layout.tsx
+++ components/with-layout.tsx
@@ -1,17 +1,25 @@
-import { useRouter } from "next/router"
+import { IconDefinition } from "@fortawesome/free-solid-svg-icons"
+import { NextRouter, useRouter } from "next/router"
 import React, { ComponentType, useMemo } from "react"
 
 import { Layout } from "./Layout"
 
 type LayoutProps = {
   heading?: string | (() => JSX.Element)
-  actions?: Array<any>
+  actions?: Array<{
+    ariaLabel: string
+    icon: IconDefinition
+    onClick: () => Promise<boolean>
+    colorScheme: string
+  }>
 }
 
 // eslint-disable-next-line @typescript-eslint/ban-types
 const withLayout = <T extends {}>(
   Component: ComponentType<T>,
-  layoutConfig?: LayoutProps | ((p: T, api: any) => LayoutProps)
+  layoutConfig?:
+    | LayoutProps
+    | ((p: T, api: { router: NextRouter }) => LayoutProps)
 ) => {
   const ComponentWithLayout = (props: T) => {
     const router = useRouter()
